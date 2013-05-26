6e70034c2ee6ecdd16f335f0b07980f1e3236761
bakey <bakey1985@qq.com>
2013-05-26 23:27:55 +0800

    s

---
+++ src/com/androidhive/musicplayer/SongsManager.java
@@ -0,0 +1,69 @@
+package com.androidhive.musicplayer;
+
+import java.io.File;
+import org.apache.commons.*;
+
+import java.io.FilenameFilter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.util.Log;
+import android.app.Activity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.impl.client.BasicResponseHandler;
+
+public class SongsManager {
+	// SDCard Path
+	final String MEDIA_PATH = new String("/sdcard/");
+	private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	
+	// Constructor
+	public SongsManager(){
+		
+	}
+	
+	/**
+	 * Function to read all mp3 files from sdcard
+	 * and store the details in ArrayList
+	 * */
+	public ArrayList<HashMap<String, String>> getPlayList( Activity context ){
+		/*String url = "http://" + context.getString(R.string.remote_host ) + "/list.json";
+		try {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpRequest = new HttpGet( url ); 
+			String content = httpclient.execute( httpRequest , new BasicResponseHandler() );
+		}catch (IOException e) {
+	　　       e.printStackTrace();
+	　　  }*/
+		HashMap<String, String> song = new HashMap<String, String>();
+		song.put("songTitle", "两个旋和三个旋");
+		song.put("songPath", "http://211.147.15.167/song/23gs.mp3");
+		songsList.add( song );
+		return songsList;
+		/*File home = new File(MEDIA_PATH);
+
+		if (home.listFiles(new FileExtensionFilter()).length > 0) {
+			for (File file : home.listFiles(new FileExtensionFilter())) {
+				HashMap<String, String> song = new HashMap<String, String>();
+				song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
+				song.put("songPath", file.getPath());
+				
+				// Adding each song to SongList
+				songsList.add(song);
+			}
+		}*/
+		// return songs list array
+
+	}
+	
+	/**
+	 * Class to filter files which are having .mp3 extension
+	 * */
+	class FileExtensionFilter implements FilenameFilter {
+		public boolean accept(File dir, String name) {
+			return (name.endsWith(".mp3") || name.endsWith(".MP3"));
+		}
+	}
+}
---
+++ src/com/androidhive/musicplayer/common/GuideHelper.java
@@ -0,0 +1,197 @@
+package com.androidhive.musicplayer.common;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.androidhive.musicplayer.R;
+import com.androidhive.musicplayer.views.ScrollLayout;
+import com.androidhive.musicplayer.views.ScrollLayout.PageEndListener;
+
+/**
+ * @Description: 引导图助手类
+ * @author yanzw
+ * @date 2012-11-30上午11:12:46
+ */
+public class GuideHelper {
+	private Activity context;
+	private ViewGroup rootLayout;
+	private ScrollLayout scrollLayout;
+	//private int[] guideResIds = {R.drawable.guide_help1, R.drawable.guide_help2, R.drawable.guide_help3, R.drawable.guide_help4};
+	private int[] guideResIds = {
+				R.drawable.welcome_item_1, 
+				R.drawable.welcome_item_2 ,
+				R.drawable.welcome_item_3 ,};
+	private static final String GUIDE_VERSION_NAME = "GUIDEVERSION";
+	private static final int  GUIDE_VERSION_CODE = 4;
+	
+	public GuideHelper(Context context){
+		this.context = (Activity)context;
+		createGuideLayout();
+		initGuideView();
+	}
+	
+	/**
+	 * @Description: 创建引导图层
+	 * @param @return
+	 * @return ViewGroup
+	 * @throws
+	 */
+	private void createGuideLayout(){
+		ViewGroup rootView = (ViewGroup) context.getWindow().getDecorView();
+		LayoutInflater lf = context.getLayoutInflater();
+		try {
+			rootLayout = (ViewGroup) lf.inflate(R.layout.guide_helper, null);
+		}catch( InflateException e) {
+			e.printStackTrace();
+			Log.d( "GuideHelper" , "inflate exception ");
+			return ;
+		}
+		scrollLayout = (ScrollLayout) rootLayout.findViewById(R.id.scroll_layout);
+		rootView.addView(rootLayout);
+	}
+	
+	/**
+	 * @Description: 初始化引导视图
+	 * @param 
+	 * @return void
+	 * @throws
+	 */
+	public void initGuideView() {
+		for(int resId : guideResIds){
+			scrollLayout.addView(makeGuideView(resId));
+		}
+		View iv = makeGuideView( R.drawable.welcome_item_5_2_num );
+		scrollLayout.addView( iv );
+		/*View iv = (View)this.context.findViewById( R.layout.guide_last );
+		if ( iv != null ) {
+			scrollLayout.addView( iv );
+		}else {
+			Log.d( "GuideHelper" , "find view by id failed ");
+		}*/
+		/*View top_right_btn = rootLayout.findViewById(R.id.top_right_btn);
+		top_right_btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				closeGuide();
+			}
+		});*/
+		scrollLayout.setPageEndListener(new PageEndListener(){
+
+			@Override
+			public void scrollEnd() {
+				closeGuide();
+			}
+			
+		});
+	}
+	
+	/**
+	 * @Description: 生成每个引导视图
+	 * @param @param resId
+	 * @param @return
+	 * @return View
+	 * @throws
+	 */
+	public View makeGuideView(int resId){
+		ImageView guideView = new ImageView(context);
+		guideView.setImageResource(resId);
+		guideView.setPadding(10, 10, 10, 10);
+		return guideView;
+	}
+	
+	/**
+	 * @Description: 打开引导层
+	 * @param 
+	 * @return void
+	 * @throws
+	 */
+	public void openGuide(){
+		if(guideCheck()){
+			rootLayout.setVisibility(View.VISIBLE);
+		}
+	}
+	
+	/**
+	 * @Description: 关闭引导层
+	 * @param 
+	 * @return void
+	 * @throws
+	 */
+	public void closeGuide(){
+		
+		AlphaAnimation alphaAnim = new AlphaAnimation(1.0f, 0.2f);
+		alphaAnim.setDuration(500);
+		ScaleAnimation scaleAnim = new ScaleAnimation(1.0f, 0.0f, 1.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+		scaleAnim.setDuration(500);
+		AnimationSet AnimSet = new AnimationSet(false);
+		AnimSet.setDuration(500);
+		AnimSet.addAnimation(scaleAnim);
+		AnimSet.addAnimation(alphaAnim);
+		
+		AnimSet.setAnimationListener(new AnimationListener(){
+
+			@Override
+			public void onAnimationStart(Animation animation) {
+				
+			}
+
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				rootLayout.clearAnimation();
+				rootLayout.setVisibility(View.GONE);
+				saveGuideVersion();
+			}
+
+			@Override
+			public void onAnimationRepeat(Animation animation) {
+				
+			}
+			
+		});
+		
+		rootLayout.startAnimation(AnimSet);
+	}
+	
+	/**
+	 * @Description: 保存引导版本记录
+	 * @param 
+	 * @return void
+	 * @throws
+	 */
+	private void saveGuideVersion() {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		Editor edit = sp.edit();
+		edit.putInt(GUIDE_VERSION_NAME, GUIDE_VERSION_CODE);
+		edit.commit();
+	}
+	
+	/**
+	 * @Description: 检测引导图版本，判断是否启动引导
+	 * @param @return
+	 * @return boolean
+	 * @throws
+	 */
+	private boolean guideCheck(){
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		int guideVer = sp.getInt(GUIDE_VERSION_NAME, 0);
+		if(GUIDE_VERSION_CODE > 0 && GUIDE_VERSION_CODE > guideVer){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
